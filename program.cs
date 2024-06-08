14efd9cfd59ce13b10eb0aad2f3346b5fb8fb9dd
safacanmetin <safacanmetin@gmail.com>
2024-06-08 18:09:41 +0300

    Initial

---
+++ program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+class Greeting
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello! What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Nice to meet you, " + name + "!");
+
+        Console.WriteLine("Press any key to close...");
+        Console.ReadKey();
+    }
+}
